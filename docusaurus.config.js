7e9cfc3f30624ce57bfe9b4412734f3efed1b5d9
thomas <banrieen@163.com>
2024-10-27 00:29:37 +0800

    fix github pages conf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/favicon-robot.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://banrieen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'banrieen', // Usually your GitHub org/user name.
   projectName: 'banrieen.github.io', // Usually your repo name.
-
+  deploymentBranch: "master",
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
